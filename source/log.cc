1d88ede97f2262e0dcdd47c26561923ebba10f6e
Bruno Ribeiro <brunexgeek@users.noreply.github.com>
2021-10-30 19:39:03 -0300

    Improve log writing

--- source/log.cc
+++ source/log.cc
@@ -4,7 +4,10 @@
 #include <stdarg.h>
 #include <limits.h>
 #include <cstdlib>
+#include <cstring>
 #include <string>
+#include <iostream>
+#include <iomanip>
 
 Log *Log::instance = nullptr;
 
@@ -50,4 +53,108 @@ void Log::write(
 	vfprintf(output, format, args);
 	va_end(args);
 	fflush(output);
+}
+
+const char *Buffer::Iterator::next()
+{
+    std::cout << "next()\n";
+    // find the end of the current string
+    while (*c_ != 0)
+    {
+        ++c_;
+        if (c_ >= e_) c_ = s_;
+        if (c_ == m_) return c_ = e_;
+    }
+    // skip the null-terminator
+    ++c_;
+    if (c_ >= e_) c_ = s_;
+    if (*c_ == 0) c_ = e_;
+    return c_;
+}
+
+std::string Buffer::Iterator::operator*() const
+{
+    std::string out;
+    const char *ptr = c_;
+    while (*ptr != 0)
+    {
+        out += *ptr;
+        ++ptr;
+        if (ptr >= e_) ptr = s_;
+    }
+    return out;
+}
+
+Buffer::Buffer( size_t size ) : size_(size)
+{
+    if (size_ < 16) size_ = 16;
+    ptr_ = new(std::nothrow) char[size_]();
+    cur_ = ptr_ + 1;
+}
+
+Buffer::~Buffer()
+{
+    delete[] ptr_;
+}
+
+void Buffer::append( const char *value )
+{
+    auto len = strlen(value);
+    if (len + 1 >= size_) return;
+    for (const char *c = value; *c != 0; ++c) append(*c);
+    append('\0');
+
+    if (*cur_ != 0)
+    {
+        // remove the string being corrupted
+        char *p = cur_;
+        while (*p != 0)
+        {
+            *p = 0;
+            ++p;
+            if (p >= ptr_ + size_) p = ptr_;
+        }
+    }
+}
+
+void Buffer::append( char value )
+{
+    *cur_ = value;
+    ++cur_;
+    if (cur_ >= ptr_ + size_) cur_ = ptr_;
+}
+
+void Buffer::clear()
+{
+    memset(ptr_, 0, size_);
+    cur_ = ptr_ + 1;
+}
+
+void Buffer::dump() const
+{
+    for (size_t i = 0; i < size_; ++i)
+    {
+        if (i > 0 && (i % 8) == 0) std::cout << '\n';
+        std::cout << ((ptr_ + i == cur_) ? '!' : ' ') << ((ptr_[i] == 0) ? '_' : ptr_[i]) << ' ';
+    }
+    std::cout << "\n\n";
+}
+
+Buffer::Iterator Buffer::begin() const
+{
+    const char *ptr = cur_ + 1;
+    if (ptr >= ptr_ + size_) ptr = ptr_;
+
+    while (*ptr == 0 && ptr != cur_)
+    {
+        ++ptr;
+        if (ptr >= ptr_ + size_) ptr = ptr_;
+    }
+    if (ptr == cur_) return end();
+    return Iterator(ptr_, ptr_+ size_, ptr);
+}
+
+Buffer::Iterator Buffer::end() const
+{
+    return Iterator(ptr_, ptr_+ size_, ptr_ + size_);
 }
--- source/log.hh
+++ source/log.hh
@@ -10,6 +10,49 @@
 #define LOG_MESSAGE(...)    Log::instance->write(false, __VA_ARGS__)
 #define LOG_TIMED(...)      Log::instance->write(true, __VA_ARGS__)
 
+class Buffer
+{
+    public:
+        class Iterator
+        {
+            public:
+                using iterator_category = std::input_iterator_tag;
+                using difference_type   = std::ptrdiff_t;
+                using value_type        = char;
+                using pointer           = const char*;
+                using reference         = const char&;
+
+                Iterator(pointer s, pointer e, pointer c) : s_(s), e_(e), c_(c), m_(c) {}
+                std::string operator*() const;
+                pointer operator->() const { return c_; }
+                Iterator& operator++() { next(); return *this; }
+                Iterator operator++(int) { Iterator tmp = *this; next(); return tmp; }
+                friend bool operator== (const Iterator& a, const Iterator& b) { return a.c_ == b.c_; };
+                friend bool operator!= (const Iterator& a, const Iterator& b) { return a.c_ != b.c_; };
+                pointer next();
+
+            private:
+                pointer s_;
+                pointer e_;
+                pointer c_;
+                pointer m_;
+        };
+
+        Buffer( size_t size );
+        virtual ~Buffer();
+        void append( const char *value );
+        void clear();
+        void dump() const;
+        Iterator begin() const;
+        Iterator end() const;
+
+    private:
+        char *ptr_;
+        size_t size_;
+        char *cur_;
+
+        void append( char value );
+};
 
 class Log
 {
